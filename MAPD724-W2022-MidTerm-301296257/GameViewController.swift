e966ca86b871674ce73834cc1659abe3ce5ceaea
Amrik Singh <113277554+Amrik-khaira@users.noreply.github.com>
2023-03-05 07:41:04 -0500

    Add files via upload

---
+++ MAPD724-W2022-MidTerm-301296257/GameViewController.swift
@@ -0,0 +1,160 @@
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    // Instance Variables
+    var currentScene: GKScene?
+    
+    @IBOutlet weak var LivesLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet weak var StartLabel: UILabel!
+    @IBOutlet weak var StartButton: UIButton!
+    @IBOutlet weak var EndLabel: UILabel!
+    @IBOutlet weak var RestartButton: UIButton!
+    @IBOutlet weak var lblStartTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var btnStartTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var lblEndTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var btnRestartTopConstraint: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presentStartScene()
+        CollisionManager.gameViewController = self
+        if UIDevice.current.orientation.isPortrait {
+            self.portraitOrientation()
+        } else {
+            self.LandescapeOrientation()
+        }
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+//
+        if self.StartButton.isHidden == true && self.RestartButton.isHidden == true{
+            self.setScene(sceneName: "GameScene")
+        }
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        var text=""
+        switch UIDevice.current.orientation{
+        case .portrait:
+            text="Portrait"
+            self.portraitOrientation()
+            
+        case .portraitUpsideDown:
+            text="PortraitUpsideDown"
+        case .landscapeLeft:
+            text="LandscapeLeft"
+            self.LandescapeOrientation()
+        case .landscapeRight:
+            text="LandscapeRight"
+            self.LandescapeOrientation()
+        default:
+            text="Another"
+        }
+        print("You have moved: \(text)")
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func updateLivesLabel()
+    {
+        LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+    }
+    
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
+    }
+    
+    func setScene(sceneName: String) -> Void {
+        currentScene = GKScene(fileNamed: sceneName)
+        if let scene = currentScene!.rootNode as! SKScene? {
+            scene.scaleMode = .aspectFill
+            if let view = self.view as! SKView? {
+                view.presentScene(scene)
+                
+                view.ignoresSiblingOrder = true
+            }
+        }
+    }
+    
+    func presentStartScene()
+    {
+        ScoreLabel.isHidden = true
+        LivesLabel.isHidden = true
+        StartLabel.isHidden = false
+        StartButton.isHidden = false
+        setScene(sceneName: "StartScene")
+    }
+    
+    func presentEndScene()
+    {
+        ScoreLabel.isHidden = true
+        LivesLabel.isHidden = true
+        RestartButton.isHidden = false
+        EndLabel.isHidden = false
+        setScene(sceneName: "EndScene")
+    }
+    
+    @IBAction func StartButton_Pressed(_ sender: UIButton) {
+        ScoreLabel.isHidden = false
+        LivesLabel.isHidden = false
+        StartLabel.isHidden = true
+        StartButton.isHidden = true
+        // Initialize the Lives and Score
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        setScene(sceneName: "GameScene")
+    }
+    
+    @IBAction func RestartButton_Pressed(_ sender: UIButton)
+    {
+        ScoreLabel.isHidden = false
+        LivesLabel.isHidden = false
+        RestartButton.isHidden = true
+        EndLabel.isHidden = true
+        // Initialize the Lives and Score
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        setScene(sceneName: "GameScene")
+        
+    }
+    
+    
+}
+
+
+
+extension GameViewController {
+    func portraitOrientation() {
+        self.lblStartTopConstraint.constant = 120
+        self.btnStartTopConstraint.constant = 80
+        self.lblEndTopConstraint.constant = 120
+        self.btnRestartTopConstraint.constant = 40
+        
+    }
+    
+    
+    func LandescapeOrientation() {
+        self.lblStartTopConstraint.constant = 20
+        self.btnStartTopConstraint.constant = 40
+        self.lblEndTopConstraint.constant = 20
+        self.btnRestartTopConstraint.constant = 20
+    }
+    
+    
+}
+
